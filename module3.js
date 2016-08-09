af7ba215138182cc9bf89669cdf159cbb854729f
Jake Malwitz <malwitzjake@comcast.net>
2016-08-09 13:11:35 -0500

    rearranged output

--- module3.js
+++ module3.js
@@ -1,7 +1,7 @@
 var convertNum = require('./module2');
 
 var account = function() {
-  return "Account Balance\n" + convertNum();
+  return "Account Balance:\n" + convertNum() + '\n';
 }
 
 module.exports = account;
